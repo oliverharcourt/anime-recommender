fca46e56238614d38933ef0061e0aeb96364522f
Oliver Harcourt <96662442+oliverharcourt@users.noreply.github.com>
2024-05-28 22:44:40 +0200

    Add first autoencoder implementation

--- src/models/autoencoder.py
+++ src/models/autoencoder.py
@@ -2,12 +2,49 @@ import torch.nn as nn
 
 class Autoencoder(nn.Module):
 
-    def __init__(self, config):
+    def __init__(self, config=None):
         super().__init__()
 
         self.config = config
 
         self.encoder = nn.Sequential(
-            nn.Linear(config['input_size'], 128),
-            nn.ReLU(),
+            nn.Linear(config['input_dim'], 4096),
+            nn.Dropout(0.3),
+            nn.GELU(),
+            nn.BatchNorm1d(4096, affine=True),
+            nn.Linear(4096, 2048),
+            nn.GELU(),
+            nn.Linear(2048, 1024),
+            nn.GELU(),
+            nn.Linear(1024, 512),
+            nn.Dropout(0.3),
+            nn.GELU(),
+            #nn.BatchNorm1d(512, affine=True),
+            nn.Linear(512, 256),
+            nn.GELU()
         )
+
+        self.decoder = nn.Sequential(
+            nn.Linear(256, 512),
+            nn.GELU(),
+            #nn.BatchNorm1d(512, affine=True),
+            nn.Linear(512, 1024),
+            nn.Dropout(0.3),
+            nn.GELU(),
+            nn.Linear(1024, 2048),
+            nn.GELU(),
+            nn.Linear(2048, 4096),
+            nn.Dropout(0.3),
+            nn.GELU(),
+            nn.BatchNorm1d(4096, affine=True),
+            nn.Linear(4096, config['input_dim'])
+        )
+
+    def forward(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
+    
+    def generate_embeddings(self, x):
+
+        return self.encoder(x)
